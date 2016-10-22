ccaebf2161b4aed0484a4a6c3ef793630f8c3ac6
UJM4 <UJM4@US14LAP00002.com>
2016-10-22 01:22:33 -0400

    initial commit

---
+++ GitProject/src/com/sai/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sai.demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello World 2");
+		
+	}
+
+}
